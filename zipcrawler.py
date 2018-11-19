477d25a2a61be61adf19132ae6f8ad55774f91df
Peter <panatale1@gmail.com>
2018-11-18 20:55:05 -0500

    gets compression working

--- zipcrawler.py
+++ zipcrawler.py
@@ -1,15 +1,22 @@
 from ansible_vault import Vault
 import argparse
+from datetime import datetime
+import gzip
 import os
 from re import findall
 import sendgrid
 from sendgrid.helpers.mail import *
+from shutil import copyfileobj
+
+INCOMPRESSIBLE_FILES = ['jpg', 'pdf', 'wma', 'mp3', 'avi', 'mp4', 'gz', 'zip']
 
 
 class ZipCrawler:
     def __init__(self, directory, threshold, email, dry_run):
         self.directory = directory
-        self.threshold = threshold
+        if self.directory[-1] != '/':
+            self.directory += '/'
+        self.threshold = int(threshold)
         self.email = email
         self.dry_run = dry_run
         self.compressed_files = []
@@ -18,10 +25,53 @@ class ZipCrawler:
 
     def run(self):
         self.crawl_directory()
+        self.print_results()
+        if not self.dry_run:
+            self.send_email()
 
     def crawl_directory(self):
         for dirpath, dirnames, filenames in os.walk(self.directory):
-            print(filenames)
+            for filename in filenames:
+                self.compress(filename)
+            break
+
+    def compress(self, filename):
+        uncompressed_size = os.stat('{}{}'.format(self.directory, filename)).st_size
+        print("{} analyzing {}{}".format(datetime.now(), self.directory, filename))
+        if uncompressed_size < self.threshold:
+            print("{} skipping {}{} -- smaller than threshold".format(
+                datetime.now(), self.directory, filename))
+            self.uncompressed_files.append(filename)
+        else:
+            with open('{}{}'.format(self.directory, filename), 'rb') as input_file:
+                with gzip.open('{}{}.gz'.format(self.directory, filename), 'wb') as output_file:
+                    copyfileobj(input_file, output_file)
+            compressed_size = os.stat('{}{}.gz'.format(self.directory, filename)).st_size
+            if self.calculate_compression_ratio(uncompressed_size, compressed_size) < 10:
+                print("{} skipping {}{} -- less than 10% compression".format(
+                    datetime.now(), self.directory, filename)) 
+                self.uncompressed_files.append(filename)
+                os.remove('{}{}.gz'.format(self.directory, filename))
+            else:
+                print("{} compressing {}{}".format(datetime.now(), self.directory, filename))
+                self.compressed_files.append(filename)
+                self.space_savings += (uncompressed_size - compressed_size)
+                if self.dry_run:
+                    os.remove('{}{}.gz'.format(self.directory, filename))
+                else:
+                    os.remove('{}{}'.format(self.directory, filename))
+
+    def calculate_compression_ratio(self, uncompressed, compressed):
+        return (1 - (compressed/uncompressed)) * 100
+
+    def print_results(self):
+        print("These files were compressed:")
+        print(" ".join(self.compressed_files))
+        print("")
+        print("These files were not compressed:")
+        print(" ".join(self.uncompressed_files))
+        print("")
+        print("Total disk savings: {} bytes".format(self.space_savings))
 
     def send_email(self):
         vault = Vault('Frankenstein')
@@ -31,7 +81,14 @@ class ZipCrawler:
         to_email = Email(self.email)
         subject = "AppNexus Crawler Compression Results"
         # TODO: Update content
-        content = Content("text/plain", "this is test content")
+        content = "These files were compressed:\n"
+        for filename in self.compressed_files:
+            content += '{}\n'.format(filename)
+        content += "\nThese files were not compressed:\n"
+        for filename in self.uncompressed_files:
+            content += '{}\n'.format(filename)
+        content += '\nTotal disk savings: {} bytes'.format(self.space_savings)
+        content = Content("text/plain", content)
         mail = Mail(from_email, subject, to_email, content)
         mail.reply_to = Email('panatale1@gmail.com')
         response = sg.client.mail.send.post(request_body=mail.get())
